2a20ad24a9d19f75d2481531205ef7096feb4484
Lucas Oliveira <lucasilva999@hotmail.com.br>
2019-08-15 14:43:41 -0300

    Alterações na View e na Lógica

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,47 @@ const Registro = require('../models/Registro');
 //Monstrando todos os registros
 router.get('/', async (req, res)=> {
     const data = await Registro.find({});
-    res.send(data);
+    try {
+        let json = preparaJSON(data);
+        res.send({indicadores: json});
+
+    }catch(err) {
+        console.log(`Erro: ${err}`);
+    }
 })
 
 //Mostrando registros listados por OE
 router.get('/:oe', async (req, res)=> {
     let oe = req.params.oe;
     const data = await Registro.find({oe});
-    res.send(data);
+    let json = preparaJSON(data);
+    res.send({indicadores: json});
 })
 
 //Mostrando registros listados por OE/Período
 router.get('/:oe/:periodo', async (req, res)=> {
-    let oe = req.params.oe;
-    let periodo = req.params.periodo;
+    let { oe, periodo } = req.params;
     const data = await Registro.find({oe, periodo});
-    res.send(data);
+    let json = preparaJSON(data);
+    res.send({indicadores: json});
 })
+
+//Função que prepara o JSON no formato para ser enviado
+function preparaJSON(data) {
+    let array = [];
+        data.forEach(indicador => {
+            let json = {
+                oe: indicador.oe,
+                nome: indicador.indicador,
+                fonte: indicador.fonte,
+                periodos: {
+                    ano: indicador.periodo,
+                    valor: indicador.dado
+                }
+            }
+            array.push(json);  
+    })
+    return array;
+}
 
 module.exports = router;
