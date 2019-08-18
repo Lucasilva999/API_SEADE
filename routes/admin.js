5bad8ff7776831dc2a1b49e8aee0e9e80c70cc7b
Lucasilva999 <Lucasilva999@hotmail.com.br>
2019-08-17 22:29:46 -0300

    Lógica para o Read do CRUD adicionada

--- routes/admin.js
+++ routes/admin.js
@@ -13,12 +13,28 @@ router.get('/cadastro', (req, res)=> {
     res.render('cadastro.handlebars');
 })
 
+/*
+.then(res.render('registros.handlebars', {variavel, registro}))
+*/
+
 //Página para visualização de informações cadastradas
 router.get('/registros', async (req, res)=> {
     try{
-        await Registro.find({}).sort({oe: "asc"}).then(registros => {
-        res.render('registros.handlebars', {registros});
-    })
+        const dataRegistro = await Registro.find({}).sort({oe_num: "asc"});
+        const dataVariavel = await Variavel.find({}).sort({"periodo.ano": "asc"});
+        //Formatando a exibição de Registros e Variáveis
+        let registros = preparaRegistros(dataRegistro);
+        let variaveis = preparaVariaveis(dataVariavel);
+        //Inserindo as Variáveis corretas em cada Registro para a exibição
+        variaveis.forEach(variavel => {
+            registros.forEach(registro => {
+                if(variavel.oe_origem == registro.oe_num) {
+                    delete(variavel.oe_origem);
+                    registro.variaveis.push(variavel);
+                }
+            })
+        })
+        res.render('registros.handlebars', {registros})
  
     }catch(err) {
         res.send(`<p>Erro: ${err}</p>`);
@@ -78,6 +94,40 @@ function defineOE(oe_num) {
             break;
     }
     return oe;
+}
+
+//Função que prepara os Registros no formato para ser enviado
+function preparaRegistros(data) {
+    let array = [];
+    data.forEach(indicador => {
+        let json = {
+            oe_num: indicador.oe_num,
+            oe: indicador.oe,
+            variaveis: []
+        }
+        array.push(json);
+        
+    })
+    return array;
+}
+
+//Função que prepara as Variáveis no formato ideal para serem enviadas
+function preparaVariaveis(data) {
+    let array = [];
+    data.forEach(indicador => {
+        let json = {
+            oe_origem: indicador.oe_origem,
+            indicador: indicador.indicador,
+            fonte: indicador.fonte,
+            periodo: {
+                ano: indicador.periodo.ano,
+                valor: indicador.periodo.valor
+            }
+        }
+        array.push(json);
+        
+    })
+    return array;
 }
 
 module.exports = router;
