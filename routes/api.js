0b4fc8b9e332c1c99d89f5843c6b1c763a55b9cc
Lucas Oliveira <lucasilva999@hotmail.com.br>
2019-08-19 13:16:22 -0300

    Ajuste nas rotas da aplicação

--- routes/api.js
+++ routes/api.js
@@ -30,68 +30,6 @@ router.get('/', async (req, res)=> {
     }
 })
 
-//Rota para deletar Registros
-router.get('/delete/registro/:id', async (req, res) => {
-    try{
-        //Exclui o Registro do BD
-        const registro = await Registro.findOne({"_id": req.params.id});
-        await Registro.deleteOne({"_id": req.params.id});
-        //Exclui cada uma da Variaveis atribuídas a esse Registro
-        registro.variaveis.forEach(async variavel => {
-            await Variavel.deleteOne({"_id": variavel});
-        })
-
-        res.redirect('/admin/registros');
-        
-    }catch(err){
-        console.log(`Erro ao deletar registro: ${err}`);
-    }
-})
-
-//Rota para deletar Variáveis
-router.get('/delete/variavel/:id', async (req, res) => {
-    try{
-        const registros = await Registro.find({});
-        const variavel = await Variavel.findOne({"_id": req.params.id});
-        let idVariavel = variavel._id.toString();
-
-        registros.forEach(registro => {
-            registro.variaveis.forEach(async variavel => {
-                //Procurando no array do Registro pela variável correspondente
-                if(variavel == idVariavel) {
-                    //Excluindo o ID da Váriavel armazenada no array do Registro
-                    await Registro.findOneAndUpdate({variaveis: idVariavel}, 
-                    {$pull: {variaveis: variavel}}, {new: true});
-                }
-            })
-        })
-        //Deletando a Variável correspondente
-        await Variavel.deleteOne({"_id": req.params.id});
-        res.redirect('/admin/registros');
-        
-    }catch(err){
-        console.log(`Erro ao deletar registro: ${err}`);
-    }
-})
-
-//Rota Para Editar Variáveis
-router.post('/update', async (req, res)=> {
-    let { _id, ano, valor, fonte, indicador } = req.body;
-    
-    try {
-        const variavel = await Variavel.findOne({_id});
-        variavel.indicador = indicador;
-        variavel.fonte = fonte;
-        variavel.periodo.ano = ano;
-        variavel.periodo.valor = valor;
-        await variavel.save();
-        res.redirect('/admin/registros');
-
-    }catch(err) {
-        res.send(`<p>Erro: ${err}</p>`);
-    }
-})
-
 //Mostrando registros listados por OE
 router.get('/:oe_num', async (req, res)=> {
     let oe_num = req.params.oe_num;
